4ab90da8110f3d3e61c8086def70c010f99d621b
siddharthgupta <siddharth20it028@gmail.com>
2022-01-06 03:35:05 +0530

    lesson 2

---
+++ app/src/main/java/com/example/swosh/LeagueActivity.kt
@@ -0,0 +1,11 @@
+package com.example.swosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}

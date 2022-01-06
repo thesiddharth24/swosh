6ab1777a94a2f8fcddbe1879c332fc36fe78e4e8
siddharthgupta <siddharth20it028@gmail.com>
2022-01-06 17:10:51 +0530

    lesson-3

--- app/src/main/java/com/example/swosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swosh/LeagueActivity.kt
@@ -1,6 +1,6 @@
 package com.example.swosh
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class LeagueActivity : AppCompatActivity() {
--- app/src/main/java/com/example/swosh/MainActivity.kt
+++ app/src/main/java/com/example/swosh/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.swosh
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val leagueintent =Intent(this,LeagueActivity::class.java)
+            startActivity(leagueintent)
+        }
     }
 }
